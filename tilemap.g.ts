16b5bccf9fba09ded365174b234c51759e330b63
NBG <71229222+nil-bego@users.noreply.github.com>
2020-09-20 11:39:08 +0200

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,33 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`10001000010101010c0c0c0c0c0c0c0c0c0c0c0c0101010c0c0c0c0c0c0c0c0c0c0c0c0c01010c0c0c0c0c0c0c0c0c0c0c0c0c0c010c0c0c0c0c0c010c010c0c0c0c0c0c0c0c0c0c0c010c050505050c0c0c0c0c0c0c0c0c0c0105050205050c0c0c0c0c0c0c050505050502020c050c0c0c0c0c0c0c0505050c05020505050c010c0c0c0c020c05050105050502050c01010c0c0c03020c050b01020502020c02020c0c0c05050c050505020505050505060c0c0c07050c0c05060c0c0c050505060c0c0c0705050505060c0c0c050505060c0c0c0a08080808090c0c0c0c0105050c0c0c0c0c0c0c0c0c0c0c0c0c0c0505050c0c0c0c0c0c0c0c0c0c0c0c0c0c0c040c`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 2 2 2 2 2 . . . . 
+. . . . . 2 2 . . . . 2 . . . . 
+. 2 2 2 2 2 . . 2 . . 2 . . . . 
+. 2 . . . . . 2 2 2 . 2 . . . . 
+. 2 . . . 2 . 2 . . . 2 . . . . 
+2 2 2 . . 2 . . . 2 . 2 . . . . 
+2 . 2 2 . . 2 2 . 2 2 2 2 2 2 . 
+2 . . 2 . . . 2 . . . . . . 2 . 
+2 . . 2 2 . . 2 2 2 . . . . 2 . 
+2 . . . . . . 2 . 2 . . . . 2 . 
+2 . . . . . . 2 . 2 2 2 . . 2 2 
+2 2 2 2 2 2 2 2 . . . 2 . . . 2 
+. . . . . . . . . . . 2 2 2 . 2 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass3,sprites.dungeon.doorClosedNorth,myTiles.tile1,sprites.castle.tilePath5,sprites.castle.tilePath6,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.castle.tilePath9,sprites.castle.tilePath7,sprites.castle.tilePath3,sprites.castle.tileGrass1], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
